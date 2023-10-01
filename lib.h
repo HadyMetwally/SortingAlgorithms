bac72d61040c3cbf3c02f5a19e24d0d77d692a64
HADY <HHHADY1510@gmail.com>
2023-10-01 20:38:35 +0300

    lib.h and define ascending and descending  applying function like macros to take array from user and display array

--- lib.h
+++ lib.h
@@ -4,3 +4,17 @@
 #include<iterator>
 #include<functional>
 
+#define ascending  0
+#define descending 1
+
+#define display(arr) do{\
+                       for(auto it : arr)\
+                       {std::cout<<it<<"\n";}\
+                       std::cout<<"\n\n" ;\
+                       }while(0) ;
+
+#define input(arr) do{\
+                       for(auto & it : arr)\
+                       {std::cout<<"Enter\n";std::cin>>it;}\
+                        std::cout<<"\n\n" ;\
+                       }while(0) ;
